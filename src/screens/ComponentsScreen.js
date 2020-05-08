3b5718085432cea37df80ed4c05201052ee5debf
petercolella <peter.joseph.colella@gmail.com>
2020-05-07 23:17:22 -0400

    Section 2 complete

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
 
 const ComponentsScreen = () => {
-  return <Text style={styles.textStyle}>This is the Components screen</Text>;
+  const name = 'Peter';
+
+  return (
+    <View>
+      <Text style={styles.textStyle}>Getting started with React Native</Text>
+      <Text style={styles.subHeader}>My name is {name}</Text>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   textStyle: {
-    fontSize: 30
+    fontSize: 45
+  },
+  subHeader: {
+    fontSize: 20
   }
 });
 
