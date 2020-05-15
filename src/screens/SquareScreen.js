a5fa44f3ab9a20730460c7c2d38160f961becc4a
petercolella <peter.joseph.colella@gmail.com>
2020-05-15 19:36:36 -0400

    Section 6 53

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -3,71 +3,81 @@ import { View, Text, StyleSheet } from 'react-native';
 import ColorCounter from '../components/ColorCounter';
 import Separator from '../components/Separator';
 
-const COLOR_INCREMENT = 15;
+const COLOR_INCREMENT = 20;
 
 const SquareScreen = () => {
   const [red, setRed] = useState(0);
   const [green, setGreen] = useState(0);
   const [blue, setBlue] = useState(0);
 
-  console.log(red);
-
-  const setColor = (color, increase = true) => {
+  const setColor = (color, plusOrMinus) => {
+    const change = COLOR_INCREMENT * plusOrMinus;
     switch (color) {
-      case 'red' && increase:
-        setRed(red + COLOR_INCREMENT > 255 ? 255 : red + COLOR_INCREMENT);
+      case 'red':
+        const newRed = red + change;
+        const correctedRed = newRed > 255 ? 255 : newRed < 0 ? 0 : newRed;
+        setRed(correctedRed);
         break;
-      case 'red' && !increase:
-        setRed(red - COLOR_INCREMENT < 0 ? 0 : red - COLOR_INCREMENT);
+      case 'green':
+        const newGreen = green + change;
+        const correctedGreen =
+          newGreen > 255 ? 255 : newGreen < 0 ? 0 : newGreen;
+        setGreen(correctedGreen);
         break;
-      case 'green' && increase:
-        setGreen(green + COLOR_INCREMENT > 255 ? 255 : green + COLOR_INCREMENT);
+      case 'blue':
+        const newBlue = blue + change;
+        const correctedBlue = newBlue > 255 ? 255 : newBlue < 0 ? 0 : newBlue;
+        setBlue(correctedBlue);
         break;
-      case 'green' && !increase:
-        setGreen(green - COLOR_INCREMENT < 0 ? 0 : green - COLOR_INCREMENT);
-        break;
-      case 'blue' && increase:
-        setBlue(blue + COLOR_INCREMENT > 255 ? 255 : blue + COLOR_INCREMENT);
-        break;
-      case 'blue' && !increase:
-        setBlue(blue - COLOR_INCREMENT < 0 ? 0 : blue - COLOR_INCREMENT);
-        break;
+      default:
+        return;
     }
   };
 
   return (
     <View>
       <ColorCounter
-        onIncrease={() =>
-          setRed(red + COLOR_INCREMENT > 255 ? 255 : red + COLOR_INCREMENT)
-        }
-        onDecrease={() =>
-          setRed(red - COLOR_INCREMENT < 0 ? 0 : red - COLOR_INCREMENT)
-        }
+        onIncrease={() => setColor('red', 1)}
+        onDecrease={() => setColor('red', -1)}
         color="Red"
       />
       <ColorCounter
-        onIncrease={() => setGreen(green + COLOR_INCREMENT)}
-        onDecrease={() => setGreen(green - COLOR_INCREMENT)}
+        onIncrease={() => setColor('green', 1)}
+        onDecrease={() => setColor('green', -1)}
         color="Green"
       />
       <ColorCounter
-        onIncrease={() => setBlue(blue + COLOR_INCREMENT)}
-        onDecrease={() => setBlue(blue - COLOR_INCREMENT)}
+        onIncrease={() => setColor('blue', 1)}
+        onDecrease={() => setColor('blue', -1)}
         color="Blue"
       />
       <Separator />
       <View
+        style={styles.squareStyle}
         style={{
-          height: 150,
-          width: 150,
+          ...styles.squareStyle,
           backgroundColor: `rgb(${red}, ${green}, ${blue})`
         }}
-      />
+      >
+        <Text style={styles.textStyle}>Red: {red}</Text>
+        <Text style={styles.textStyle}>Green: {green}</Text>
+        <Text style={styles.textStyle}>Blue: {blue}</Text>
+      </View>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  squareStyle: {
+    height: 150,
+    width: 150,
+    flexDirection: 'column',
+    justifyContent: 'center'
+  },
+  textStyle: {
+    textAlign: 'center',
+    color: 'white'
+  }
+});
 
 export default SquareScreen;
