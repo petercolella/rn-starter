8c2dbd05554938ef90dd8dc16d484d5bdb131996
petercolella <peter.joseph.colella@gmail.com>
2020-05-17 19:03:22 -0400

    Section 6 59

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,19 +1,33 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { Text, View, Button, StyleSheet } from 'react-native';
 import Separator from '../components/Separator';
 
-const CounterScreen = () => {
-  const [count, setCount] = useState(0);
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'increase':
+      return { ...state, count: state.count + action.payload };
+    case 'decrease':
+      return { ...state, count: state.count - action.payload };
+    default:
+      return state;
+  }
+};
 
-  const handleChange = increment => {
-    setCount(count + increment);
-  };
+const CounterScreen = () => {
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
+  const { count } = state;
 
   return (
     <View>
-      <Button title="Increase" onPress={() => handleChange(1)} />
+      <Button
+        title="Increase"
+        onPress={() => dispatch({ type: 'increase', payload: 1 })}
+      />
       <Separator />
-      <Button title="Decrease" onPress={() => handleChange(-1)} />
+      <Button
+        title="Decrease"
+        onPress={() => dispatch({ type: 'decrease', payload: 1 })}
+      />
       <Text>Current Count: {count}</Text>
     </View>
   );
