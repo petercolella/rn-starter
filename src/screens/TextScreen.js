089ecb3b8771bf606b4bb3d3d01d5e01366ddcaf
petercolella <peter.joseph.colella@gmail.com>
2020-05-17 19:35:14 -0400

    Section 6 64

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet, TextInput } from 'react-native';
 
 const TextScreen = () => {
+  const [name, setName] = useState('');
   return (
     <View>
+      <Text>Enter Name:</Text>
       <TextInput
         style={styles.input}
         autoCapitalize="none"
         autoCorrect={false}
+        value={name}
+        onChangeText={text => setName(text)}
       />
+      <Text>My name is {name}</Text>
     </View>
   );
 };
